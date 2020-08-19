69c5dcbd3e5fabfd71ab4471b22a45ef82c8f8b4
Ayo Akindele <a.akindele01@gmail.com>
2020-08-19 10:53:37 -0600

    added the backend functionality for the hashtags

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,50 +1,69 @@
 class MessagesController < ApplicationController
     def index
-      @messages = Message.all
+        @messages = Message.all
     end
   
     def new
-      @message = Message.new
+        @message = Message.new
     end
   
     def create
-      @message = Message.create(message_params)
-  
-      if @message.save
-        redirect_to @message
-      else
-        render :new
-      end
+        @message = Message.create(message_params)
+
+        if @message.save
+            redirect_to @message
+        else
+            render :new
+        end
     end
   
     def show
-      @message = Message.find(params[:id])
+        @message = Message.find(params[:id])
     end
   
     def edit
-      @message = Message.find(params[:id])
+        @message = Message.find(params[:id])
     end
   
     def update
-      @message = Message.find(params[:id])
-  
-      if @message.update(message_params)
-        redirect_to @message
-      else
-        render :edit
-      end
+        @message = Message.find(params[:id])
+    
+        if @message.update(message_params)
+            redirect_to @message
+        else
+            render :edit
+        end
     end
   
     def destroy
-      @message = Message.find(params[:id])
-  
-      @message.destroy
-      redirect_to messages_path
+        # can't use Active Record Callback for destroy 
+        # because it violates foreign key constraints
+        
+        @message = Message.find(params[:id])
+
+        # retain tag ids even after @message.hashtags is cleared
+        tag_ids = []
+        @message.hashtags.each do |tag|
+            tag_ids << tag.id
+        end
+
+        @message.hashtags.clear
+
+        # use tag ids to delete hashtags that @message has
+        tag_ids.each do |tag_id|
+            # don't delete a @message hashtag that is being used in another message
+            unless MessageHashtag.find_by(hashtag_id: tag_id)
+                Hashtag.find(tag_id).destroy
+            end
+        end
+
+        @message.destroy
+        redirect_to messages_path
     end
   
     private
     def message_params  
-      params.require(:message).permit(:title, :body)
+        params.require(:message).permit(:title, :body)
     end
   
   end  
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,64 @@ class Message < ApplicationRecord
     validates :body, presence: true
     has_many :message_hashtags
     has_many :hashtags, through: :message_hashtags
+
+    # Active Record Callback for create
+    after_create do
+        message = Message.find_by(id: self.id)
+        
+        # create a hash for message's hashtags
+        @tags = Hash.new 0
+        message.body.split().each do |word| 
+            if word.start_with?"#" 
+                @tags[word] += 1
+            end 
+        end 
+
+        # add each hashtag to Hashtag and add its count to MessageHashtag
+        @tags.each do |key, value|
+            tag_name = Hashtag.find_or_create_by(name: key.downcase.delete('#'))
+            message.hashtags << tag_name
+            
+            tag_count = MessageHashtag.find_by(hashtag_id: tag_name.id, message_id: message.id)
+            tag_count.update!(hash_count: value)
+        end
+    end
+
+    # Active Record Callback for update
+    after_update do
+        message = Message.find_by(id: self.id)
+
+        # retain tag ids even after message.hashtags is cleared
+        tag_ids = []
+        message.hashtags.each do |tag|
+            tag_ids << tag.id
+        end
+
+        message.hashtags.clear
+
+        # use tag ids to delete hashtags that message has
+        tag_ids.each do |tag_id|
+            # don't delete a message hashtag that is being used in another message
+            unless MessageHashtag.find_by(hashtag_id: tag_id)
+                Hashtag.find(tag_id).destroy
+            end
+        end
+
+        # create a hash for message's hashtags
+        @tags = Hash.new 0
+        message.body.split().each do |word| 
+            if word.start_with?"#" 
+                @tags[word] += 1
+            end 
+        end 
+
+        # add each hashtag to Hashtag and add its count to MessageHashtag
+        @tags.each do |key, value|
+            tag_name = Hashtag.find_or_create_by(name: key.downcase.delete('#'))
+            message.hashtags << tag_name
+            
+            tag_count = MessageHashtag.find_by(hashtag_id: tag_name.id, message_id: message.id)
+            tag_count.update!(hash_count: value)
+        end
+    end
 end
